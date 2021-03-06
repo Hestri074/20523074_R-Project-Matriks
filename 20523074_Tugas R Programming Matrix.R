a9979b047678b7e936032f96ce9218d0d7fc7657
Hestri074 <20523074@students.uii.ac.id>
2021-03-06 14:28:11 +0700

    Tugas Matematika Lanjut

---
+++ 20523074_Tugas R Programming Matrix.R
@@ -0,0 +1,15 @@
+values <-sample(1:1000, 100, FALSE)
+values
+G <-matrix(values, 10, 10, TRUE)
+G
+H <-t(G)
+H
+J <-G+H
+J
+det(G)
+det(H)
+det(J)
+K <-cbind(G[ ,1:5], J[ ,1:5])
+K
+G1 <- solve(G)
+G%*%G1
